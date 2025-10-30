d1859ec78f9c12fdfc8b670195bd8a634df63bcc
beeynow <beeynow@gmail.com>
2025-10-30 19:32:56 +0100

    fixed ads size

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -159,7 +159,7 @@ export default function Contact() {
           <div className="my-10 flex justify-center">
             <ins
               className="adsbygoogle"
-              style={{ display: "block", width: "100%", height: 90 }}
+              style={{ display: "block", width: "100%", height: 40 }}
               data-ad-client="ca-pub-7858868028312077"
               data-ad-slot="3376852857"
               data-ad-format="auto"
--- src/pages/Conversions.tsx
+++ src/pages/Conversions.tsx
@@ -1060,6 +1060,17 @@ export default function Conversions() {
                 </div>
               </TabsContent>
             ))}
+            {/* ✅ Adsense block */}
+            <div className="my-10 flex justify-center">
+              <ins
+                className="adsbygoogle"
+                style={{ display: "block", width: "100%", height: 40 }}
+                data-ad-client="ca-pub-7858868028312077"
+                data-ad-slot="3376852857"
+                data-ad-format="auto"
+                data-full-width-responsive="true"
+              ></ins>
+            </div>
           </Tabs>
 
           {/* Privacy Notice */}
@@ -1087,7 +1098,7 @@ export default function Conversions() {
           <div className="my-10 flex justify-center">
             <ins
               className="adsbygoogle"
-              style={{ display: "block", width: "100%", height: 90 }}
+              style={{ display: "block", width: "100%", height: 40 }}
               data-ad-client="ca-pub-7858868028312077"
               data-ad-slot="3376852857"
               data-ad-format="auto"

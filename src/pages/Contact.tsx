229dfbb431b1aecc7cc6512e92ee9b12766176c0
beeynow <beeynow@gmail.com>
2025-10-30 19:40:17 +0100

    fixed ads size

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -159,12 +159,10 @@ export default function Contact() {
           <div className="my-10 flex justify-center">
             <ins
               className="adsbygoogle"
-              style={{ display: "block", width: "100%", height: 50 }}
+              style={{ display: "inline-block", width: 300, height: 50 }}
               data-ad-client="ca-pub-7858868028312077"
               data-ad-slot="3376852857"
-              data-ad-format="auto"
-              data-full-width-responsive="true"
-            ></ins>
+            />
           </div>
         </main>
 
